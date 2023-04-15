c2aa848d713eb1427f209e758bee0c6a0f8ec334
Javier Carrasco <javierdeveloper08@gmail.com>
2023-04-15 17:40:05 -0400

    Agregando el componente Restart Button

--- src/components/Shift-panel/ShiftPanel.jsx
+++ src/components/Shift-panel/ShiftPanel.jsx
@@ -1,10 +1,11 @@
-import "./panel.css";
+import ButtonRestart from "../Button-restart/ButtonRestart";
+import "./shiftPanel.css";
 
 export default function ShiftPanel() {
   return (
     <aside className="panel">
       <h2 className="panel__message">Shift: ⭕</h2>
-      <button className="panel__restart">Restart</button>
+      <ButtonRestart />
     </aside>
   );
 }
