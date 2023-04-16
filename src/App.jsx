2dfa0a80b8643bea1dc2af32d79ba237f5f1c58d
Javier Carrasco <javierdeveloper08@gmail.com>
2023-04-16 12:09:07 -0400

    Agregando estado de las filas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import ShiftPanel from "./components/Shift-panel/ShiftPanel";
 import SocialMedia from "./components/Social-media/SocialMedia";
 import WinnerPanel from "./components/Winner-panel/WinnerPanel";
+import TicTacToe from "./components/Tic-tac-toe/TicTacToe";
 
-function App() {}
+function App() {
+  const [top, setTop] = useState(["", "", ""]);
+  const [center, setCenter] = useState(["", "", ""]);
+  const [bottom, setBottom] = useState(["", "", ""]);
+
+  return (
+    <>
+      <main className="container">
+        <ShiftPanel />
+        <TicTacToe top={top} center={center} bottom={bottom} />
+      </main>
+      <footer className="foo">
+        <SocialMedia />
+      </footer>
+    </>
+  );
+}
 
 export default App;
