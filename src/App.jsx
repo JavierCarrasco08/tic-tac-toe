48d6445a43b1e3ac77798520116dc2111367f509
Javier Carrasco <javierdeveloper08@gmail.com>
2023-04-16 16:59:25 -0400

    Insertando componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { RestartContext } from "./context/restart";
 import "./App.css";
 import ShiftPanel from "./components/Shift-panel/ShiftPanel";
 import SocialMedia from "./components/Social-media/SocialMedia";
@@ -9,16 +10,22 @@ function App() {
   const [top, setTop] = useState(["", "", ""]);
   const [center, setCenter] = useState(["", "", ""]);
   const [bottom, setBottom] = useState(["", "", ""]);
-
+  function handleClickRestart() {
+    setTop(["", "", ""]);
+    setCenter(["", "", ""]);
+    setBottom(["", "", ""]);
+  }
   return (
     <>
-      <main className="container">
-        <ShiftPanel />
-        <TicTacToe top={top} center={center} bottom={bottom} />
-      </main>
-      <footer className="foo">
-        <SocialMedia />
-      </footer>
+      <RestartContext.Provider value={handleClickRestart}>
+        <main className="container">
+          <ShiftPanel />
+          <TicTacToe top={top} center={center} bottom={bottom} />
+        </main>
+        <footer className="foo">
+          <SocialMedia />
+        </footer>
+      </RestartContext.Provider>
     </>
   );
 }
--- src/components/Button-restart/ButtonRestart.jsx
+++ src/components/Button-restart/ButtonRestart.jsx
@@ -1,5 +1,12 @@
+import { useContext } from "react";
 import "./buttonRestart.css";
+import { RestartContext } from "../../context/restart";
 
 export default function ButtonRestart() {
-  return <button className="restart">Restart</button>;
+  const Restart = useContext(RestartContext);
+  return (
+    <button className="restart" onPointerDown={Restart}>
+      Restart
+    </button>
+  );
 }
