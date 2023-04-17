a4a17a75798486bf5e084f4a20de4ffb64bcccfa
Javier Carrasco <javierdeveloper08@gmail.com>
2023-04-17 15:28:32 -0400

    Refactorizando Estado

--- src/components/Tic-tac-toe/TicTacToe.jsx
+++ src/components/Tic-tac-toe/TicTacToe.jsx
@@ -1,26 +1,38 @@
 import "./ticTacToe.css";
 
-export default function TicTacToe({ top, center, bottom }) {
+export default function TicTacToe({ cells, onAdd }) {
+  let top = cells.filter((e, index) => index < 3);
+  let center = cells.filter((e, index) => index >= 3 && index < 6);
+  let bottom = cells.filter((e, index) => index >= 6);
   return (
     <table className="tic-tac-toe">
       <tbody>
         <tr className="tic-tac-toe__top">
           {top.map((tic, index) => (
-            <td className="tic-tac-toe__td--top" key={index}>
+            <td
+              className="tic-tac-toe__td--top"
+              key={index}
+              onPointerDown={() => onAdd("top", index)}>
               {tic}
             </td>
           ))}
         </tr>
         <tr className="tic-tac-toe__center">
           {center.map((tic, index) => (
-            <td className="tic-tac-toe__td--center" key={index}>
+            <td
+              className="tic-tac-toe__td--center"
+              key={index}
+              onPointerDown={() => onAdd("center", index)}>
               {tic}
             </td>
           ))}
         </tr>
         <tr className="tic-tac-toe__bottom">
           {bottom.map((tic, index) => (
-            <td className="tic-tac-toe__td--bottom" key={index}>
+            <td
+              className="tic-tac-toe__td--bottom"
+              key={index}
+              onPointerDown={() => onAdd("bottom", index)}>
               {tic}
             </td>
           ))}
