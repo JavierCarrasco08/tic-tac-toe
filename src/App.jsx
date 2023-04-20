38900ac8f784869bcf7dd2a4aee640ff0350bfc1
Javier Carrasco <javierdeveloper08@gmail.com>
2023-04-20 16:12:25 -0400

    Estructura de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { searchCells } from "./utils/searchCells";
 import { RestartContext } from "./context/restart";
 import "./App.css";
 import ShiftPanel from "./components/shift-panel/ShiftPanel";
 import SocialMedia from "./components/social-media/SocialMedia";
 import WinnerPanel from "./components/winner-panel/WinnerPanel";
-import TicTacToe from "./components/tic-tac-toe/TicTacToe";
+import TicTacToe from "./components/Tic-tac-toe/TicTacToe";
 
 function App() {
   const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
   const [turn, setTurn] = useState(true);
+  const [win, setWin] = useState(null);
+  useEffect(() => {
+    let result = searchCells(cells, 0);
+    if (result) {
+      setWin(result);
+    }
+  }, [cells]);
   function handleClickRestart() {
     setCells(["", "", "", "", "", "", "", "", ""]);
     setTurn(true);
+    setWin(null);
   }
   function handleClickAdd(id) {
     setCells(
@@ -28,10 +37,14 @@ function App() {
   return (
     <>
       <RestartContext.Provider value={handleClickRestart}>
-        <main className="container">
-          <ShiftPanel turn={turn ? "⭕" : "❌"} />
-          <TicTacToe cells={cells} onAdd={handleClickAdd} />
-        </main>
+        {win ? (
+          <WinnerPanel winner={win} />
+        ) : (
+          <main className="container">
+            <ShiftPanel turn={turn ? "⭕" : "❌"} />
+            <TicTacToe cells={cells} onAdd={handleClickAdd} />
+          </main>
+        )}
         <footer className="foo">
           <SocialMedia />
         </footer>
