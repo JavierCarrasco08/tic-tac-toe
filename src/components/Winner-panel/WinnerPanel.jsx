bbe83e28a43cb3c4bc1093242dc0411a077bf797
Javier Carrasco <javierdeveloper08@gmail.com>
2023-04-20 16:12:05 -0400

    Agregando propiedad

--- src/components/Winner-panel/WinnerPanel.jsx
+++ src/components/Winner-panel/WinnerPanel.jsx
@@ -1,11 +1,11 @@
 import ButtonRestart from "../button-restart/ButtonRestart";
 import "./winnerPanel.css";
 
-export default function WinnerPanel() {
+export default function WinnerPanel({ winner }) {
   return (
     <aside className="winner">
       <h2 className="winner__message">Congratulations</h2>
-      <h1 className="winner__player">❌</h1>
+      <h1 className="winner__player">{winner}</h1>
       <ButtonRestart />
     </aside>
   );
