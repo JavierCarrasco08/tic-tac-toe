f998553d190ce564cf2ffabbd2523b4c3ea052fb
Javier Carrasco <javierdeveloper08@gmail.com>
2023-04-20 16:20:06 -0400

    Utilizando cssAnimate en winner-panel

--- src/components/Winner-panel/WinnerPanel.jsx
+++ src/components/Winner-panel/WinnerPanel.jsx
@@ -1,9 +1,10 @@
 import ButtonRestart from "../button-restart/ButtonRestart";
 import "./winnerPanel.css";
+import "animate.css";
 
 export default function WinnerPanel({ winner }) {
   return (
-    <aside className="winner">
+    <aside className="winner animate__animated animate__zoomInDown">
       <h2 className="winner__message">Congratulations</h2>
       <h1 className="winner__player">{winner}</h1>
       <ButtonRestart />
